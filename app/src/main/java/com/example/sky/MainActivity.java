60ea9343a41b2af4e33fb841729a8be8505c2e3c
Fátima <ffernandezp22@fpcoruna.afundacion.org>
2024-01-18 11:44:46 +0100

    Hecho drawer, con los fragments correspondientes vacíos

--- app/src/main/java/com/example/sky/MainActivity.java
+++ app/src/main/java/com/example/sky/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.sky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Provisional
+        Intent myIntent = new Intent(context, Drawer.class);
+        context.startActivity(myIntent);
+
     }
 }
---
+++ app/src/main/java/com/example/sky/ui/configuracion/ConfiguracionViewModel.java
@@ -0,0 +1,19 @@
+package com.example.sky.ui.configuracion;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ConfiguracionViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ConfiguracionViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
