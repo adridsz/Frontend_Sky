68d712c66be67494a727faae022408c9b9e5525d
adridsz <adiazs22@fpcoruna.afundacion.org>
2024-01-29 17:21:54 +0100

    Solucionar crasheo constante al abrir la app

--- app/src/main/java/com/example/sky/MainActivity.java
+++ app/src/main/java/com/example/sky/MainActivity.java
@@ -1,28 +1,23 @@
 package com.example.sky;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create new fragment and transaction
-        EditarCarpetaFragment newFragment = new EditarCarpetaFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //Provisional
+        Intent myIntent = new Intent(context, Drawer.class);
+        context.startActivity(myIntent);
 
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack
-        transaction.replace(R.id.fragment_container, newFragment);
-        transaction.addToBackStack(null);
-
-        // Commit the transaction
-        transaction.commit();
     }
 }
--- app/src/main/java/com/example/sky/ui/cerrar_sesion/CerrarSesionFragment.java
+++ app/src/main/java/com/example/sky/ui/cerrar_sesion/CerrarSesionFragment.java
@@ -23,7 +23,7 @@ public class CerrarSesionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cerrarsesion, container, false);
+        return inflater.inflate(R.layout.fragment_cerrar_sesion, container, false);
     }
 
 
