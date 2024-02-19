69ef62b27b62df6830fd2d2d93e69a2080bd9c3e
Fátima <ffernandezp22@fpcoruna.afundacion.org>
2024-02-19 12:54:11 +0100

    Creada actividad de Imagen

---
+++ app/src/main/java/com/example/sky/Imagen.java
@@ -0,0 +1,16 @@
+package com.example.sky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Imagen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagen);
+
+
+    }
+}
